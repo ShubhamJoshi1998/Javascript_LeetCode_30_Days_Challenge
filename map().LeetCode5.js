8dd2dfbc3268b13128010928cc1d5b428b6f80a0
Shubham <shivam306joshi@gmail.com>
2023-08-14 17:14:07 +0530

    Given an integer array arr and a mapping function fn, return a new array with a transformation applied to each element.

---
+++ map().LeetCode5.js
@@ -0,0 +1,19 @@
+// Given an integer array arr and a mapping function fn, return a new array with a transformation applied to each element.
+// The returned array should be created such that returnedArray[i] = fn(arr[i], i).
+// Please solve it without the built-in Array.map method.
+
+// Input: arr = [1,2,3], fn = function plusone(n) { return n + 1; }
+// Output: [2,3,4]
+// Explanation:
+// const newArray = map(arr, plusone); // [2,3,4]
+// The function increases each value in the array by one. 
+
+
+// Input: arr = [1,2,3], fn = function plusI(n, i) { return n + i; }
+// Output: [1,3,5]
+// Explanation: The function increases each value by the index it resides in.
+
+
+var map = function(arr, fn) {
+    return arr.map(fn)
+};
